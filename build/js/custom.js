1eb7bdfd0c498aee03dc0c06a13023fbc016e2bd
dysabel12 <dysabel12@gmail.com>
2017-01-21 11:41:27 -0500

    take-photos
    :dancer:

--- build/js/custom.js
+++ build/js/custom.js
@@ -284,3 +284,62 @@ if (typeof NProgress != 'undefined') {
         NProgress.done();
     });
 }
+var count = 0;
+function readURL(input) {
+    var url = input.value;
+    var ext = url.substring(url.lastIndexOf('.') + 1).toLowerCase();
+    if (input.files && input.files[0]&& (ext == "gif" || ext == "png" || ext == "jpeg" || ext == "jpg")) {
+        var reader = new FileReader();
+
+        var newblock="<div class='col-md-3'>"+
+            "<div class='thumbnail'>"+
+            "<img class='img-cover' id='evidencias-"+ count +"' name='evidencias[]' src='#' alt='your image' />"+
+            "</div>"+
+            "</div>";
+        $("#gallery-ticket").append(newblock);
+        reader.onload = function (e) {
+            $('#evidencias-'+count).attr('src', e.target.result);
+            count++;
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }else{
+         $('#img').attr('src', '/assets/no_preview.png');
+    }
+}
+
+// Grab elements, create settings, etc.
+var video = document.getElementById('video');
+// Elements for taking the snapshot
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+$('#show-camera').click(function(){
+    // Get access to the camera!
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+    });
+}
+});
+$('#snap').click(function(){
+           context.drawImage(video, 0, 0, 400, 300);
+            var newblock="<div class='col-md-3'>"+
+            "<div class='thumbnail'>"+
+            "<img class='img-cover' id='evidencias-"+ count +"' name='evidencias[]' src='#' alt='your image' />"+
+            "</div>"+
+            "</div>";
+        $("#gallery-ticket").append(newblock);
+        $('#evidencias-'+count).attr('src', canvas.toDataURL());
+           count++;
+        });
+
+
+$("#file-1").change(function(){ 
+        readURL(this);
+    });
+
+
+
