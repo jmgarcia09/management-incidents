e80a3c16384a92fd549349f7cf34f7b408d65e57
dysabel12 <dysabel12@gmail.com>
2017-01-22 23:10:02 -0500

    se aumentan paginas de mantenimiento
    :dancer:

--- build/js/custom.js
+++ build/js/custom.js
@@ -313,16 +313,41 @@ var video = document.getElementById('video');
 // Elements for taking the snapshot
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
-
+var localstream;
 $('#show-camera').click(function(){
     // Get access to the camera!
     if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     // Not adding `{ audio: true }` since we only want video now
     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        localstream=stream;
         video.src = window.URL.createObjectURL(stream);
         video.play();
     });
-}
+    }else if(navigator.getUserMedia) { // Standard
+    navigator.getUserMedia({ video: true }, function(stream) {
+        localstream=stream;
+        video.src = stream;
+        video.play();
+    }, errBack);
+    } else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+        navigator.webkitGetUserMedia({ video: true }, function(stream){
+            localstream=stream;
+            video.src = window.webkitURL.createObjectURL(stream);
+            video.play();
+        }, errBack);
+    } else if(navigator.mozGetUserMedia) { // Mozilla-prefixed
+        navigator.mozGetUserMedia({ video: true }, function(stream){
+            localstream=stream;
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+        }, errBack);
+    }
+
+});
+$('#close-camera').click(function(){
+      video.pause();
+  video.src = "";
+  localstream.getTracks()[0].stop();
 });
 $('#snap').click(function(){
            context.drawImage(video, 0, 0, 400, 300);
@@ -341,5 +366,10 @@ $("#file-1").change(function(){
         readURL(this);
     });
 
-
+         $('.date-picker').daterangepicker({
+            singleDatePicker: true,
+            calender_style: "picker_4"
+          }, function(start, end, label) {
+            console.log(start.toISOString(), end.toISOString(), label);
+          });
 
