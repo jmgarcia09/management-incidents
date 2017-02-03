cf03fd6cc4559f60b1d68c38e487c0d159fff6a1
dysabel12 <dysabel12@gmail.com>
2017-02-02 20:59:10 -0500

    update!

--- vendors/switchery/dist/switchery.js
+++ vendors/switchery/dist/switchery.js
@@ -1706,7 +1706,7 @@ Switchery.prototype.setPosition = function (clicked) {
     this.element.checked = false;
     this.switcher.style.boxShadow = 'inset 0 0 0 0 ' + this.options.secondaryColor;
     this.switcher.style.borderColor = this.options.secondaryColor;
-    this.switcher.style.backgroundColor = (this.options.secondaryColor !== defaults.secondaryColor) ? this.options.secondaryColor : '#fff';
+    this.switcher.style.backgroundColor = (this.options.secondaryColor !== defaults.secondaryColor) ? this.options.secondaryColor : '#26b99a';
     this.jack.style.backgroundColor = (this.options.jackSecondaryColor !== this.options.jackColor) ? this.options.jackSecondaryColor : this.options.jackColor;
     this.setSpeed();
   }
